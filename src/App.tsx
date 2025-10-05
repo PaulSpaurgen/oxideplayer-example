ffddc61f92f0d80774789083fa542b44b7b7f8a3
paul <gspaurgen@gmail.com>
2025-10-05 17:40:56 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh', width: '100vw' }}>
       <h1>Oxide Player Demo</h1>
       <div style={{ width: '50vw' }}>
-        <Videoplayer videoSrc="https://oxideplayer.s3.us-east-1.amazonaws.com/4ksongtestvideo.mp4  " showProcessingSnackbar />
+        <Videoplayer videoSrc="https://oxideplayer.s3.us-east-1.amazonaws.com/4ksongtestvideo.mp4  " showProcessingSnackbar={false} />
       </div>
     </div>
   )

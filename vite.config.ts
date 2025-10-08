0cc74a44a38bd8173e7e2fe475ead453c42df564
paul <gspaurgen@gmail.com>
2025-10-08 21:51:32 +0530

    updated vite config for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import wasm from 'vite-plugin-wasm'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    wasm(),
+    topLevelAwait()
+  ],
   optimizeDeps: {
     exclude: ['oxideplayer']  // Don't pre-bundle oxideplayer
   },
@@ -11,5 +17,9 @@ export default defineConfig({
     fs: {
       allow: ['..']  // Allow serving files from node_modules
     }
+  },
+  worker: {
+    format: 'es',
+    plugins: () => [wasm(), topLevelAwait()]
   }
 })
